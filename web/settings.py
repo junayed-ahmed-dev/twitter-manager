d5371c4f435917aeacbcae05f20e392f336d79c7
Junayed Ahmed <junayed@ualberta.ca>
2026-03-04 06:07:29 +0000

    Fix allowed hosts for deployment

--- web/settings.py
+++ web/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-5i!_##ajrc@41ly1z6x-%!o+w_r8#5=qrbv@=%0+%-sn=ab==f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
